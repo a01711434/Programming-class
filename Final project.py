d0fc049979b48d3c5a2ee5b18f6f95d566ff3a79
a01711434 <142354586+a01711434@users.noreply.github.com>
2023-10-13 21:30:07 -0600

    Add files via upload

--- Final project.py
+++ Final project.py
@@ -1,87 +1,21 @@
+from strings import rosacea, hidratacion, acne, skincare, exfoliacion, rejuvenecimiento, cuerpo, cabello
 from time import sleep
 import os
+'''LISTAS'''
+opciones_si_no=["si","no"]
+opciones_main=["skincare", "cuidado del cabello"]
+opciones_skincare=["cara","cuerpo","regresar","salir"]
+opciones_cara=["acné","rosácea","hidratación","exfoliación","rejuvenecimiento","regresar","salir"]
+opciones_hidratacion=["base agua","serúm","crema"]
+opciones_exfoliacion=["grasa","mixta","seca","sensible"]
+opciones_cuerpo=["higiene personal","exfoliación","hidratación","protección solar","cuidado de las uñas",
+"cuidado de los pies","depilación","nutrición y ejercicio","descanso adecuado"]
+opciones_cabello=["rizado","ondulado","lacio"]
 
-'''
-Listas
-'''
-lista=["si","no"]
-lista1=["skincare", "cuidado del cabello"]
-lista2=["cara","cuerpo","regresar","salir"]
-lista3=["acné","rosácea","hidratación","exfoliación","rejuvenecimiento","regresar","salir"]
-lista4=["base agua","serúm","crema"]
+matriz_validacion=[opciones_si_no,opciones_main,opciones_skincare,opciones_cara,opciones_hidratacion,
+opciones_exfoliacion,opciones_cuerpo,opciones_cabello]
 
-'''
-INFORMACIÓN
-'''
-#Definición skincare y cuidado del cabello
-skincare_i=("Skincare:\n"
-"Es el cuidado que le damos a nuestra piel para mantenerla saludable.\n"
-"Se asocia con las rutinas que seguimos y los productos que usamos.\n"
-"Para que la piel de nuestro rostro o cuerpo luzca fresca y sana.\n"
-"\nCuidado del Cabello:\n"
-"El cuidado del cabello son cuidados que se aplican al cabello.\n"
-"Su finalidad es mantener la salud y buena apariencia del mismo.")
-
-acne_i=("El acné es una afección frecuente de la piel (cutánea) que ocurre cuando los folículos pilosos debajo de esta se obstruyen.\n"
-"La grasa y las células muertas de la piel tapan los poros "
-"y puede haber brotes de lesiones (a menudo llamados granos o espinillas).\n"
-"Para la mayoría de las personas, el acné tiende a desaparecer al llegar a los 30 años, "
-"pero algunas otras continúan teniendo este problema de la piel aun cuando llegan a los 40 o 50 años.\n\n"
-"Un tratamiento promedio dura 21 días y tiene un promedio de 7 días de mejora.\n"
-"Si sigues un tratamiento para el acné, te tomará aproximadamente 28 días para ver mejoras en tu piel.\n")
-
-acne_t=("Aquí están los siguientes tratamientos:\n"
-"Los objetivos que se persiguen en el tratamiento de esta enfermedad son:\n"
-"\tRegular la seborrea.\n"
-"\tEvitar la obstrucción del folículo.\n"
-"\tDisminuir la población bacteriana.\n"
-"\tEvitar las cicatrices.")
-
-rosacea_i=("Es una afección común de la piel que causa rubor o enrojecimiento y vasos sanguíneos visibles en la cara.\n"
-"Además, puede producir pequeños bultos llenos de pus.\n"
-"Estos signos y síntomas pueden aparecer durante semanas o meses y luego desaparecer por un tiempo\n"
-"La rosácea puede confundirse con el acné, otros problemas de la piel o la rubicundez natural.\n\n"
-"Algunos síntomas son:\n"
-"\tRubor o enrojecimiento facial\n"
-"\tVenas visibles\n"
-"\tProtuberancias hinchadas\n"
-"\tSensación de ardor\n"
-"\tProblemas oculaes\n"
-"\tNariz agrandada")
-
-rosacea_t=("Medicamentos tópicos que reducen el rubor:\n"
-"Para la rosácea leve, el médico puede recetar una crema o un gel que se aplica "
-"en la piel afectada.\nLa brimonidina y la oximetazolina reducen el rubor "
-"dentro de las 12 horas después de su uso.\n"
-"El medicamento debe aplicarse regularmente para mantener las mejoras.\n\n"
-"Antibióticos orales:\n"
-"El médico puede recetar un antibiótico oral como doxiciclina "
-"para la rosácea moderada a grave con protuberancias y granos")
-
-rosacea_a=("Cualquier persona puede desarrollar rosácea.\n"
-"Pero es más probable que la tengas si:\n"
-"\t-Eres mujer\n"
-"\t-Tienes una piel que se quema facilmente con el sol\n"
-"\t-Tienes más de 30 años\n"
-"\t-Fumas\n"
-"\t-Tienes antecedentes familiares de rosácea")
-
-
-hidratacion_i=("Una piel hidratada es una piel feliz.\n"
-"La importancia de la hidratación facial es clave para conseguir una complexión sana y luminosa.\n"
-"Una piel hidratada pareca más joven. Los humectantes ayudan a mantener la humedad de la piel y "
-"la hidratación le añade agua.\nLa piel deshidratada puede estar seca y grasa a la vez. La piel seca "
-"se caracteriza por tener una producción baja de grasa, mientrás que la pied deshidratada simplemente "
-"le falta agua.\nCuando tu piel está deshidrata, puede estar más apagada, sin brillo, tirante y con tendencia "
-"a inflamarse, congestionarse y a tener erupciones.")
-
-exfoliacion_i=("")
-
-rejuvenecimiento_i=("")
-
-'''
-Funciones
-'''
+'''FUNCIONES'''
 #Esperar al usuario
 def inputUsuario():
     input("Pulse enter para continuar\n\n>>> ")
@@ -107,57 +41,57 @@ def opcion_cara():
         opcion=input(">>> ")
         limpiar_pantalla()
         
-        validacion=validarOpcion(opcion, lista3)
+        validacion=validarOpcion(opcion, matriz_validacion[3])
 
         if validacion[0]:
             if validacion[1]==0:
-                print(acne_i)
+                print(acne[0])
                 print("\n¿Quieres saber sobre tratamientos para el acné?\n")
                 while True:
                     opcion=input("Debes contestar con si o no\n>>> ")
                     limpiar_pantalla()
                     
-                    validacion=validarOpcion(opcion, lista)
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
 
                     if validacion[0]:
                         if validacion[1]==0:
-                            print(acne_t)
+                            print(acne[1])
                             inputUsuario()
                         limpiar_pantalla()
                         break
 
             elif validacion[1]==1:
-                print(rosacea_i)
+                print(rosacea[0])
                 print("\n¿Quieres saber sobre tratamientos para la rosácea?\n")
                 while True:
                     opcion=input("Debes contestar con si o no\n>>> ")
                     limpiar_pantalla()
                     
-                    validacion=validarOpcion(opcion, lista)
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
 
                     if validacion[0]:
                         if validacion[1]==0:
-                            print(rosacea_t)
+                            print(rosacea[1])
                         break
                 print("\n¿Quieres saber sobre factores de riesgo de la rosácea?\n")
                 while True:
                     opcion=input("Debes contestar con si o no\n>>> ")
                     limpiar_pantalla()
                     
-                    validacion=validarOpcion(opcion, lista)
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
 
                     if validacion[0]:
                         if validacion[1]==0:
-                            print(rosacea_a)
+                            print(rosacea[2])
                         break
             elif validacion[1]==2:
-                print(hidratacion_i)
+                print(hidratacion[0])
                 print("¿Te gustaría saber un método de hidratación?\n")
                 while True:
                     opcion=input("Debes contestar con si o no\n>>> ")
                     limpiar_pantalla()
                     
-                    validacion=validarOpcion(opcion, lista)
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
 
                     if validacion[0]:
                         if validacion[1]==0:
@@ -167,30 +101,112 @@ def opcion_cara():
                                 opcion=input(">>> ")
                                 limpiar_pantalla()
                                 
-                                validacion=validarOpcion(opcion, lista4)
+                                validacion=validarOpcion(opcion, matriz_validacion[4])
 
                                 if validacion[0]:
                                     if validacion[1]==0:
-                                        print(lista4[0])
+                                        print(hidratacion[1])
                                     elif validacion[1]==1:
-                                        print(lista4[1])
+                                        print(hidratacion[2])
                                     elif validacion[1]==2:
-                                        print(lista4[2])
+                                        print(hidratacion[3])
                                     break
                         break
             elif validacion[1]==3:
-                print(exfoliacion_i)
+                print(exfoliacion[0])
+                print("¿Qué tipo de piel tienes?")
+                while True:
+                    print("Las opciones son:\n-grasa\n-mixta\n-seca\n-sensible")
+                    opcion=input(">>> ")
+                    limpiar_pantalla()
+
+                    validacion=validarOpcion(opcion, matriz_validacion[5])
+
+                    if validacion[0]:
+                        tipo_cara=validacion[1]
+                        if validacion[1]==0:
+                            print(exfoliacion[1])
+                        elif validacion[1]==1:
+                            print(exfoliacion[2])
+                        elif validacion[1]==2:
+                            print(exfoliacion[3])
+                        elif validacion[1]==3:
+                            print(exfoliacion[4])
+                        break
+                print("¿Quieres conocer un tratamiento para tú tipo de piel?")
+                while True:
+                    opcion=input("Debes contestar con si o no\n>>> ")
+                    limpiar_pantalla()
+
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
+
+                    if validacion[0]:
+                        if tipo_cara==0:
+                            print(exfoliacion[5][0])
+                        elif tipo_cara==1:
+                            print(exfoliacion[5][1])
+                        elif tipo_cara==2:
+                            print(exfoliacion[5][2])
+                        elif tipo_cara==3:
+                            print(exfoliacion[5][3])
+                        break
             elif validacion[1]==4:
-                print(rejuvenecimiento_i)
+                print(rejuvenecimiento[0])
+                print("¿Desea conocer datos útiles sobre el rejuvenecimiento de la piel?")
+                while True:
+                    opcion=input("Debes contestar con si o no\n>>> ")
+                    limpiar_pantalla()
+
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
+
+                    if validacion[0]:
+                        print(rejuvenecimiento[1])
+                        print("¿Te gustaría saber sobre procedimientos de rejuvenecimiento?")
+                        while True:
+                            opcion=input("Debes contestar con si o no\n>>> ")
+                            limpiar_pantalla()
+
+                            validacion=validarOpcion(opcion, matriz_validacion[0])
+
+                            if validacion[0]:
+                                print(rejuvenecimiento[2])
+                                break
+                        break
             elif validacion[1]==5:
                 opcion_skincare()
             break
     
 def opcion_cuerpo():
-    print("El cuidado corporal se refiere a las prácticas y rutinas\
-          que una persona realiza para mantener la salud y el bienestarde\
-          su cuerpo, especialmente la piel y otras partes externas. Existen\
-          algunas prácticas comunes del cuidado corporal.")
+    print(cuerpo[0])
+    print("¿Sobre cuál quieres saber?")
+    while True:
+        opcion=input("Las prácticas son:\n-Higiene Personal\n-Exfoliación\n"
+        "-Hidratación\n-Protección Solar\n-Cuidado de las uñas\n-Cuidado de los pies\n"
+        "-Depilación\n-Nutrición y ejercicio\n-Descanso adecuado\n>>> ")
+        limpiar_pantalla()
+
+        validacion=validarOpcion(opcion, matriz_validacion[6])
+
+        if validacion[0]:
+            if validacion[1]==0:
+                print(cuerpo[1])
+            elif validacion[1]==1:
+                print(cuerpo[2])
+            elif validacion[1]==2:
+                print(cuerpo[3])
+            elif validacion[1]==3:
+                print(cuerpo[4])
+            elif validacion[1]==4:
+                print(cuerpo[5])
+            elif validacion[1]==5:
+                print(cuerpo[6])
+            elif validacion[1]==6:
+                print(cuerpo[7])
+            elif validacion[1]==7:
+                print(cuerpo[8])
+            elif validacion[1]==8:
+                print(cuerpo[9])
+            break
 
 def opcion_skincare():
     print("¿De qué tema quieres saber?\n")
@@ -199,7 +215,7 @@ def opcion_skincare():
         opcion=input(">>> ")
         limpiar_pantalla()
         
-        validacion=validarOpcion(opcion, lista2)
+        validacion=validarOpcion(opcion, matriz_validacion[2])
 
         if validacion[0]:
             if validacion[1]==0:
@@ -211,7 +227,55 @@ def opcion_skincare():
             break
 
 def opcion_cabello():
-    print("Cabello")
+    print(cabello[0])
+    print("¿Sobre cuál tipo de cabello quieres saber?")
+    while True:
+        opcion=input("Los tipos son:\n-Rizado\n-Ondulado\n-Lacio\n>>> ")
+        limpiar_pantalla()
+
+        validacion=validarOpcion(opcion, matriz_validacion[7])
+
+        if validacion[0]:
+            if validacion[1]==0:
+                print(cabello[1])
+                print("\n¿Quieres saber acerca del tratamiento de este tipo de cabello?")
+                while True:
+                    opcion=input("Debes contestar con si o no\n>>> ")
+                    limpiar_pantalla()
+
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
+
+                    if validacion[0]:
+                        if validacion[1]==0:
+                            print(cabello[2])
+                        break
+            elif validacion[1]==1:
+                print(cabello[3])
+                print("\n¿Quieres saber acerca del tratamiento de este tipo de cabello?")
+                while True:
+                    opcion=input("Debes contestar con si o no\n>>> ")
+                    limpiar_pantalla()
+
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
+
+                    if validacion[0]:
+                        if validacion[1]==0:
+                            print(cabello[4])
+                        break
+            elif validacion[1]==2:
+                print(cabello[5])
+                print("\n¿Quieres saber acerca del tratamiento de este tipo de cabello?")
+                while True:
+                    opcion=input("Debes contestar con si o no\n>>> ")
+                    limpiar_pantalla()
+
+                    validacion=validarOpcion(opcion, matriz_validacion[0])
+
+                    if validacion[0]:
+                        if validacion[1]==0:
+                            print(cabello[6])
+                        break
+            break
 
 def informacion():
     print("¿De qué tema quieres saber?\n")
@@ -219,7 +283,7 @@ def informacion():
         opcion=input("Las opciones son:\n- skincare\n- cuidado del cabello\n\n>>> ")
         limpiar_pantalla()
 
-        validacion=validarOpcion(opcion, lista1)
+        validacion=validarOpcion(opcion, matriz_validacion[1])
 
         if validacion[0]:
             if validacion[1]==0:
@@ -233,7 +297,7 @@ def informacion():
         opcion=input("Debes contestar con si o no\n>>> ")
         limpiar_pantalla()
 
-        validacion=validarOpcion(opcion, lista)
+        validacion=validarOpcion(opcion, matriz_validacion[0])
 
         if validacion[0]:
             if validacion[1]==0:
@@ -265,12 +329,12 @@ def main():
         opcion=input("Debes contestar con si o no\n>>> ")
         limpiar_pantalla()
 
-        validacion=validarOpcion(opcion, lista)
+        validacion=validarOpcion(opcion, matriz_validacion[0])
 
         #Definición skincare
         if validacion[0]:
             if validacion[1]==0:
-                print(skincare_i)
+                print(skincare)
                 inputUsuario()
                 limpiar_pantalla()
             break

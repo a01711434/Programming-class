7a5499ee4c927d118f28422340f3d2456928595e
a01711434 <142354586+a01711434@users.noreply.github.com>
2023-10-19 19:45:04 -0600

    Update strings.py

--- strings.py
+++ strings.py
@@ -10,6 +10,7 @@ skincare=("Skincare:\n"
 "El cuidado del cabello son cuidados que se aplican al cabello.\n"
 "Su finalidad es mantener la salud y buena apariencia del mismo.")
 
+#Información sobre el acné
 acne_i=("El acné es una afección frecuente de la piel (cutánea) que ocurre cuando los folículos pilosos debajo de esta se obstruyen.\n"
 "La grasa y las células muertas de la piel tapan los poros "
 "y puede haber brotes de lesiones (a menudo llamados granos o espinillas).\n"
@@ -18,6 +19,7 @@ acne_i=("El acné es una afección frecuente de la piel (cutánea) que ocurre cu
 "Un tratamiento promedio dura 21 días y tiene un promedio de 7 días de mejora.\n"
 "Si sigues un tratamiento para el acné, te tomará aproximadamente 28 días para ver mejoras en tu piel.")
 
+#Información sobre los tratamientos del acné 
 acne_t=("Aquí están los siguientes tratamientos:\n"
 "Los objetivos que se persiguen en el tratamiento de esta enfermedad son:\n"
 "\tRegular la seborrea.\n"
@@ -25,6 +27,7 @@ acne_t=("Aquí están los siguientes tratamientos:\n"
 "\tDisminuir la población bacteriana.\n"
 "\tEvitar las cicatrices.")
 
+#Información sobre la rosácea 
 rosacea_i=("Es una afección común de la piel que causa rubor o enrojecimiento y vasos sanguíneos visibles en la cara.\n"
 "Además, puede producir pequeños bultos llenos de pus.\n"
 "Estos signos y síntomas pueden aparecer durante semanas o meses y luego desaparecer por un tiempo\n"
@@ -37,6 +40,7 @@ rosacea_i=("Es una afección común de la piel que causa rubor o enrojecimiento 
 "\tProblemas oculaes\n"
 "\tNariz agrandada")
 
+#Información sobre el tratamiento del rosácea
 rosacea_t=("Medicamentos tópicos que reducen el rubor:\n"
 "Para la rosácea leve, el médico puede recetar una crema o un gel que se aplica "
 "en la piel afectada.\nLa brimonidina y la oximetazolina reducen el rubor "
@@ -46,6 +50,7 @@ rosacea_t=("Medicamentos tópicos que reducen el rubor:\n"
 "El médico puede recetar un antibiótico oral como doxiciclina "
 "para la rosácea moderada a grave con protuberancias y granos")
 
+#Información sobre como la rosácea puede aparecer 
 rosacea_a=("Cualquier persona puede desarrollar rosácea.\n"
 "Pero es más probable que la tengas si:\n"
 "\t-Eres mujer\n"
@@ -54,6 +59,7 @@ rosacea_a=("Cualquier persona puede desarrollar rosácea.\n"
 "\t-Fumas\n"
 "\t-Tienes antecedentes familiares de rosácea")
 
+#Información sobre la hidratación
 hidratacion_i=("Una piel hidratada es una piel feliz.\n"
 "La importancia de la hidratación facial es clave para conseguir una complexión sana y luminosa.\n"
 "Una piel hidratada pareca más joven. Los humectantes ayudan a mantener la humedad de la piel y "
@@ -62,6 +68,7 @@ hidratacion_i=("Una piel hidratada es una piel feliz.\n"
 "le falta agua.\nCuando tu piel está deshidrata, puede estar más apagada, sin brillo, tirante y con tendencia "
 "a inflamarse, congestionarse y a tener erupciones.")
 
+#Información sobre productos a base de agua para hidratar
 hidratacion_ba=("La clave para la hidratación es el agua.\nPrueba un gel hidratante a base de agua después "
 "de una rutina de limpieza y exfoliación fácil para recuperar la hidratación en tu cara.\n\n"
 "Algunos ejemplos son:\n-The True Cream Moisturizing Bob de Belif: "
@@ -76,6 +83,7 @@ hidratacion_ba=("La clave para la hidratación es el agua.\nPrueba un gel hidrat
 "-Bioderma Hydrabio Gel-Creme: Alisa la textura de la piel, a la vez que aporta una "
 "sensación de frescura y bienestar.\n\nEstas son algunas cremas a base de agua que son ligeras y no engrasan la piel.") 
 
+#Información sobre productos de serum para hidratar
 hidratacion_s=("El serum es un concentrado cosmético que se aplica antes de la crema hidratante "
 "por la mañana y por la noche sobre rostro, cuello y escote.\nA diferencia de la crema hidratante, el sérum lleva una concentración "
 "mayor, por lo que solo es necesario aplicar unas pequeñas gotas para que el producto "
@@ -85,10 +93,12 @@ hidratacion_s=("El serum es un concentrado cosmético que se aplica antes de la 
 "\t-Vera & the Birds Plum Up Serum\n\t-Isidin Hyaluronic Concetrate\n\t-Filorga Age-Purify Intensive\n"
 "\t-Lancôme Advanced Génifique Youth Activating Serum\n\t-L'Oréal Paris 1.5% Hyaluronic Acid Revitalift Filler Serum")
 
+#Imformación sobre cremas que hidrátan 
 hidratacion_c=("Estas cremas reparan, hidratan y protegen la piel por la noche:\n"
 "\t-Uriage 'Age Lift'\n\t-RoC 'Retinol Correxion'\n\t-Cocunat 'The Absolute'\n\t-Garnier 'Aloe Hialurónico'\n"
 "\t-Olay Retinol 24\n\t-Nivea 'Cellular Luminous'")
 
+#Información sobre la exfoliación
 exfoliacion_i=("Exfoliar la cara es una rutina completamente necesaria, una de las tareas "
 "más básicas para disfrutar de un cutis sano y sin impurezas.\n"
 "La exfoliación es una parte importante de la limpieza de la piel, ya que "
@@ -99,42 +109,52 @@ exfoliacion_i=("Exfoliar la cara es una rutina completamente necesaria, una de l
 "de exfoliar la cara es importante elegir un producto que se adapte "
 "a las necesidades de tu piel.")
 
+#Información sobre la exfoliación en piel grasa
 exfoliacion_g=("Cuando tienes piel grasa, las células muertas tienden a permanecer más tiempo.\n"
 "Por ello la exfoliación es clave.")
 
+#Información sobre la exfoliación en piel mixta
 exfoliacion_m=("Los exfoliantes para las pieles secas eliminan impurezas y además purifica la piel"
 "eliminando la aparicion de cebo y dejando la piel mate.")
 
+#Información sobre la exfoliación en piel secas.
 exfoliacion_sc=("Las pieles más secas se pueden exfoliar cada 15 días. Es importante que entiendas que no "
 "por exfoliar con frecuencia tu piel vas a conseguir una apariencia más sana y suave.")
 
+#Información sobre la exfoliación en sensible
 exfoliacion_ss=("Un exceso de producto puede provocar picor, quemazón, aumentar el enrojecimiento o los brotes de acné "
 "y en algunos casos puede producir hasta manchas.\nSe recomienda exfoliar la piel solo una vez o como máximo dos veces por "
 "semana.\nUna sustancia que es muy utilizada por su propiedad para exfoliar y ablandar las capas más superficiales "
 "es el ácido láctico, una sustancia derivada de la proteína de leche vegetal.\n"
 "El uso de este es mediante un peeling facial de ácido láctico que se aplica directamente dobre la piel.")
 
+#Tratamiento para pieles grasas
 exfoliacion_g_t=("Se recomienda someterse a un peeling de ácido salicílico, un betahidroxiácido "
 "capaz de eliminar la grasa que obstruye los poros.\nEs un tipo de exfoliación muy segura para todo "
 "tipo de pieles y sin efectos secundarios.\nTambién ayuda a reducir eficazmente las espinillas "
 "sin dejar imperfecciones o cicatrices importantes.\nSin embargo, estos peeling no deben practicarse en casa.\n"
 "Debe realizarlos en una clínica un médico especializado.")
 
+#Tratamiento para pieles mixta
 exfoliacion_m_t=("Los exfoliantes beta-hidroxiácidos (BHA) son un tipo de ácidos óleo-solubles, "
 "lo que les permite penetrar en los folículos de la piel y eliminar la suciedad y el sebo acumulado en los poros")
 
+#Tratamiento para pieles secas
 exfoliacion_sc_t=("El ácido glicólico, generalmente se usa en piel seca por su capacidad de exfoliar "
 "las células muertas y mejorar la hidratación de la piel.\nLo ideal es usarlo en productos que puedan "
 "dejarse en la piel por tiempo prolongado, esto permite ir eliminando los enlaces que "
 "mantienen unidas las células muertas en la piel.")
 
+#Tratamiento para pieles sensibles
 exfoliacion_ss_t=("Algunos enfoliantes en el mercado son:\n-Exfoliante Fisiológico Ultra Fino de la Roche Posay\n"
 "-Manuka Tree Pasta Exfoliante de Ziaja\n-Rose Sugar Scrub Exfoliante Facial de Lancôme\n"
 "-Exfoliante Suave Purificante de Avene\n-Numee Sérum Exfoliante Ácido Láctico")
 
+#Información rejuvecimiento
 rejuvenecimiento_i=("Se refiere a las diversas prácticas, productos y técnicas dirigidas a mantener una apariencia "
 "facial juvenil y saludable a medida que uno envejece.")
 
+#Datos útiles de la exfoliación
 rejuvenecimiento_d=("Cuidado de la piel:\nEs un aspecto fundamental del rejuvecimiento de la piel.\n"
 "Esto a menudo incluye limpieza, exfoliación, hidratación u la aplicación de bloqueador solar.\n"
 "La limpieza regular ayuda a eliminar la suciedad e impurezas, la exfoliación ayuda a eliminar celúlas muertas de la"
@@ -152,6 +172,7 @@ rejuvenecimiento_d=("Cuidado de la piel:\nEs un aspecto fundamental del rejuveci
 "Sueño:\nUn sueño adecuado es esencial para la regeneración y reparación de la piel.\n"
 "La falta de sueño puede llevar a ojeras y una piel de aspecto cansado.")
 
+#Rejuvenecimeinto procedimeintos
 rejuvenecimiento_p=("Procedimientos de Rejuvecimiento Facial:\n1.- Botox y rellenos dérmicos: Se utilizan para suavizar arrugas y lineas finas, asi como para restaurar "
 "volumen en áreas como los labios y las mejillas.\n2.- Lifting facial: También conocido como ritidectomia, es un procedimiento quirúrgico "
 "elimina el exceso de piel y reafirma los tejidos subayacentes para reducir la flacidez y mejorar la apariencia del rostro.\n3.- Peelings químicos: Implican la aplicación "
@@ -160,10 +181,12 @@ rejuvenecimiento_p=("Procedimientos de Rejuvecimiento Facial:\n1.- Botox y relle
 "la producción de colágeno, lo que puede mejorar la apariencia de la piel.\n5.- Terapia con láser: Los tratamientos con láser "
 "se utilizan para tratar arrugas, cicatrices, manchas solares y otros problemas de la piel mediante la estimulación del colágeno")
 
+#Información del cuerpo
 cuerpo_i=("El cuidado corporal se refiere a las prácticas y rutinas que una persona realiza para\n"
 "mantener la salud y el bienestar de su cuerpo, especialmente la piel y otras partes\n"
 "externas. Existen algunas prácticas comunes del cuidado corporal.")
 
+#Higiene del cuidado corporal
 cuerpo_higiene=("El cuidado corporal comienza con la higiene personal diaria. Esto incluye tomar\n"
 "baños o duchas regulares para mantener la piel limpia y libre de suciedad y sudor. El\n"
 "uso de jabón suave y agua tibia es esencial para no eliminar los aceites naturales de\n"
@@ -174,6 +197,7 @@ cuerpo_higiene=("El cuidado corporal comienza con la higiene personal diaria. Es
 "Personal, son la conservación y la prevención de las infecciones y de las enfermedades,\n"
 "como también el mejorar la salud física de una persona.")
 
+#Exfoliación del cuidado corporal
 cuerpo_exfoliacion=("La exfoliación se refiere a la eliminación de células muertas de la piel. Puedes\n"
 "hacerlo utilizando exfoliantes suaves o cepillos corporales. La exfoliación regular\n"
 "puede ayudar a mantener la piel suave y renovada. Esta técnica del peeling corporal\n"
@@ -186,6 +210,7 @@ cuerpo_exfoliacion=("La exfoliación se refiere a la eliminación de células mu
 "piel con la finalidad de eliminar las estrías, cicatrices, acné o cualquier tipo de\n"
 "imperfecciones.")
 
+#Hidratación corporal
 cuerpo_hidratacion=("Es importante mantener la piel hidratada para prevenir la sequedad y la\n"
 "descamación. Usar lociones o cremas hidratantes después de la ducha puede ayudar a\n"
 "retener la humedad en la piel. Mantener una correcta hidratación es fundamental para\n"
@@ -196,6 +221,7 @@ cuerpo_hidratacion=("Es importante mantener la piel hidratada para prevenir la s
 "sino una adecuada hidratación del cuerpo, dependiendo del peso, talla, edad y\n"
 "temporada de año.")
 
+#Cuidado del cuerpo con protector solar
 cuerpo_proteccionSolar=("El protector solar es un producto cuya función es la de proteger la piel impidiendo el\n"
 "paso de los rayos UV, y reduciendo su penetración con la finalidad de que no perjudiquen\n"
 "nuestros tejidos ni las células existentes en ellos. La importancia del protector solar\n"
@@ -208,9 +234,11 @@ cuerpo_proteccionSolar=("El protector solar es un producto cuya función es la d
 "causados por el sol es crucial. El uso de protector solar antes de exponerse al sol ayuda\n"
 "a prevenir el envejecimiento prematuro de la piel y reduce el riesgo de cáncer de piel.")
 
+#Cuidado de las uñas 
 cuerpo_cuidadoUñas=("Mantener las uñas limpias y recortadas es parte del cuidado corporal. También\n"
 "puedes aplicar esmalte de uñas si lo deseas.")
 
+#Cuidado de los pies
 cuerpo_cuidadoPies=("El cuidado de los pies implica mantenerlos limpios, recortar las uñas de los pies y\n"
 "usar calzado adecuado para prevenir problemas como los hongos en las uñas o los\n"
 "callos. En muchos sitios se ofrecen servicios de pedicura, tratamientos cosméticos\n"
@@ -224,6 +252,7 @@ cuerpo_cuidadoPies=("El cuidado de los pies implica mantenerlos limpios, recorta
 "Hidrata tus pies regularmente, Ocúpate de las durezas y callos, Cuida las uñas,\n"
 "Lava y seca tus pies a diario.")
 
+#Depilación corporal
 cuerpo_depilacion=("Algunas personas también incluyen el cuidado del cabello corporal en su rutina de\n"
 "cuidado corporal. Esto puede incluir el afeitado o depilación de ciertas áreas del\n"
 "cuerpo. Existen varios tipos de servicios en los centros de estética. Por alta demanda,\n"
@@ -232,7 +261,7 @@ cuerpo_depilacion=("Algunas personas también incluyen el cuidado del cabello co
 "cuchilla, con cera, con crema, con hilo, con aparatos mecánicos, con láser, con luz\n"
 "pulsada y termoquímica.")
 
-
+#Nutrición y ejercicio para el cuerpo 
 cuerpo_nutricionEjercicio=("Una parte esencial del cuidado corporal es mantener una dieta equilibrada y hacer\n"
 "ejercicio regularmente. Esto no solo beneficia a tu piel, sino a tu salud en general.\n"
 "El consumo de alimentos y bebidas saludables, junto con el ejercicio físico regular,\n"
@@ -240,7 +269,7 @@ cuerpo_nutricionEjercicio=("Una parte esencial del cuidado corporal es mantener 
 "peso, dormir bien y controlar el estrés también podrían ayudarle a evitar algunos\n"
 "problemas de salud.")
 
-
+#Descanso adecuado para el cuerpo
 cuerpo_descansoAdecuado=("El sueño adecuado es esencial para el cuidado corporal. Un buen descanso permite\n"
 "que la piel se repare y regenera. Las horas que debemos dedicar a este descanso\n"
 "varían en función de la edad. Según las recomendaciones de la Fundación Nacional\n"
@@ -249,9 +278,11 @@ cuerpo_descansoAdecuado=("El sueño adecuado es esencial para el cuidado corpora
 "para el descanso, para poder descansar se recomienda seguir un horario de sueño y,\n"
 "siempre que sea posible, mantenerlo también los fines de semana.")
 
+#Información del cuidado del cabello
 cabello_i=("El cuidado del cabello son cuidados que se aplican al cabello. Su finalidad es\n"
 "mantener la salud y buena apariencia del mismo.")
 
+#Información del cuidado del cabello rizado
 cabelloRizado_i=("Es un tipo de cabello que tiene una forma naturalmente ondulada o rizada en lugar\n"
 "de ser liso y recto. Este tipo de cabello es muy común en personas de ascendencia\n"
 "africana, afroamericana, mediterránea y de algunas otras regiones. La textura del\n"
@@ -259,6 +290,7 @@ cabelloRizado_i=("Es un tipo de cabello que tiene una forma naturalmente ondulad
 "puede parecer más grueso y voluminoso debido a la forma en que se enrollan los\n"
 "rizos.")
 
+#Tratamientos del cuidado del cabello rizado
 cabelloRizado_t=("Cuidado y mantenimiento:\n"
 "El cuidado adecuado del cabello rizado implica hidratación regular y el uso de\n"
 "productos diseñados específicamente para cabello rizado, como acondicionadores\n"
@@ -272,6 +304,7 @@ cabelloRizado_t=("Cuidado y mantenimiento:\n"
 "pueden lograrse con cabello rizado, desde rizos sueltos hasta peinados recogidos\n"
 "elegantes.")
 
+#Información del cuidado del cabello ondulado
 cabelloOndulado_i=("El cabello ondulado es un tipo de cabello que se encuentra en un lugar intermedio\n"
 "entre el cabello liso y el cabello rizado. Tiene una forma naturalmente ondulada,\n"
 "con curvas suaves en lugar de ser completamente recto o formar rizos definidos. La\n"
@@ -279,6 +312,7 @@ cabelloOndulado_i=("El cabello ondulado es un tipo de cabello que se encuentra e
 "frizzy (con frizz) y tiende a enredarse. Puedes dejar tu cabello ondulado suelto o\n"
 "peinarlo con productos como espumas o cremas para ondas.")
 
+#Tratamiento del cuidado del cabello rizado
 cabelloOndulado_t=("El cabello ondulado, al igual que cualquier otro tipo de cabello, requiere cuidados y\n"
 "tratamientos específicos para mantenerlo saludable.\n"
 "Lavado adecuado:\n"
@@ -301,6 +335,7 @@ cabelloOndulado_t=("El cabello ondulado, al igual que cualquier otro tipo de cab
 "mejor estado. Además, puedes personalizar estos consejos según las necesidades\n"
 "específicas de tu cabello y tu preferencia en cuanto al estilo.")
 
+#Información del cuidado del cabello lacio
 cabelloLacio_i=("El cabello lacio es un tipo de cabello que se caracteriza por tener una estructura\n"
 "recta y suave, sin curvas, ondas ni rizos notables. Por lo general, el cabello lacio\n"
 "tiende a verse más fino que otros tipos de cabello debido a su falta de volumen\n"
@@ -313,6 +348,7 @@ cabelloLacio_i=("El cabello lacio es un tipo de cabello que se caracteriza por t
 "suavidad y el flujo natural del cabello. Si deseas agregar más movimiento, considera\n"
 "un corte en capas largas o un flequillo.")
 
+#Tratamiento del cuidado del cabello rizado
 cabelloLacio_t=("El cabello lacio tiene sus propias necesidades de cuidado para mantenerlo saludable\n"
 "y con un aspecto hermoso,\n"
 "Lavado adecuado:\n"
@@ -334,19 +370,26 @@ cabelloLacio_t=("El cabello lacio tiene sus propias necesidades de cuidado para 
 "las necesidades específicas de tu cabello. El cabello lacio puede ser elegante y\n"
 "hermoso con el cuidado adecuado.")
 
+#Lista para almacenar las variables relacionadas con el acné
 acne=[acne_i, acne_t]
 
+#Lista para almacenar las variables relacionadas con la rosácea
 rosacea=[rosacea_i, rosacea_t, rosacea_a]
 
+#Lista para almacenar las variables relacionadas con la hidratación
 hidratacion=[hidratacion_i, hidratacion_ba, hidratacion_s, hidratacion_c]
 
+#Lista para almacenar las variables relacionadas con la exfoliación
 exfoliacion=[exfoliacion_i,exfoliacion_g,exfoliacion_m,exfoliacion_sc,exfoliacion_ss,
     [exfoliacion_g_t,exfoliacion_m_t,exfoliacion_sc_t,exfoliacion_ss_t]]
 
+#Lista para almacenar las variables relacionadas con el rejuvecimeinto
 rejuvenecimiento=[rejuvenecimiento_i,rejuvenecimiento_d,rejuvenecimiento_p]
 
+#Lista para almacenar las variables relacionadas con la cuerpo
 cuerpo=[cuerpo_i,cuerpo_higiene,cuerpo_exfoliacion,cuerpo_hidratacion,cuerpo_proteccionSolar,
     cuerpo_cuidadoUñas,cuerpo_cuidadoPies,cuerpo_depilacion,cuerpo_nutricionEjercicio,cuerpo_descansoAdecuado]
 
+#Lista para almacenar las variables relacionadas con el cabello
 cabello=[cabello_i,cabelloRizado_i,cabelloRizado_t,cabelloOndulado_i,cabelloOndulado_t,cabelloLacio_i,cabelloLacio_t]
 
